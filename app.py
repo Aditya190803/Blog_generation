dca160707a754d76c1f7c65911d1cfe2e8f61794
Aditya190803 <101453576+Aditya190803@users.noreply.github.com>
2024-06-01 20:10:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from dotenv import load_dotenv
+import os
+## Function To get response from LLAma 2 model
+load_dotenv() 
+
+# Function to initialize conversation chain with GROQ language model
+groq_api_key = os.environ['GROQ_API_KEY']
+
+# Initializing GROQ chat with provided API key, model name, and settings
+
+def getLLamaresponse(input_text,no_words,blog_style):
+
+    ### LLama3 model
+    llm = ChatGroq( groq_api_key=groq_api_key, model_name="llama3-70b-8192",temperature=0.35)
+    
+    ## Prompt Template
+
+    template="""
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+            """
+    
+    prompt=PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
+                          template=template)
+    
+    ## Generate the ressponse from the LLama 2 model
+    response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
+    print(response)
+    return response
+
+st.set_page_config(page_title="Generate Blogs",
+                    page_icon='🤖',
+                    layout='centered',
+                    initial_sidebar_state='collapsed')
+
+st.header("Generate Blogs 🤖")
+
+input_text=st.text_input("Enter the Blog Topic")
+
+## creating to more columns for additonal 2 fields
+
+col1,col2=st.columns([5,5])
+
+with col1:
+    no_words=st.text_input('No of Words')
+with col2:
+    blog_style=st.selectbox('Writing the blog for',
+                            ('Researchers','Data Scientist','Common People'),index=0)
+    
+submit=st.button("Generate")
+
+## Final response
+if submit:
+    st.write(getLLamaresponse(input_text,no_words,blog_style))
